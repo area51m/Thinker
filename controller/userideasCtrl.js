a59508d8caf6c4f378df73f74b88557e803a4d50
Dmitry <area51m@i.ua>
2016-12-11 19:59:53 +0200

    Белый дизайн

---
+++ controller/userideasCtrl.js
@@ -0,0 +1,56 @@
+var app = angular.module("Thinker", []);
+
+app.controller('userideasCtrl', function($scope, $http, $log) {
+	$scope.body = {'background-image': '#ebebeb'};
+	$scope.newIdeaShow = false;
+	$scope.shadowShow = false;
+	$scope.selectedIdeaShow = false;
+	$scope.moreIdeasBtn = false;
+
+	$scope.newIdeaShowF = function() {
+		$scope.shadowShow = true;
+		$scope.newIdeaShow = true;
+	};
+
+	$scope.selectedIdeaShowF = function(ideaId) {
+		$scope.selectedIdeaShow = true;
+		$scope.shadowShow = true;
+		$http.get("http://thinker-codeart.44fs.preview.openshiftapps.com/restapi/ideas/" + ideaId)
+		.then(function(response){
+			$scope.selectedIdea = response.data;
+        		//console.log($scope.selectedIdea);
+        	});
+		$http.get("http://thinker-codeart.44fs.preview.openshiftapps.com/restapi/likes/idea" + ideaId)
+		.then(function(response){
+			$scope.selectedIdeaLikes = response.data;
+        		//console.log($scope.selectedIdeaLikes);
+        	});
+		$http.get("http://thinker-codeart.44fs.preview.openshiftapps.com/restapi/comments/idea" + ideaId)
+		.then(function(response){
+			$scope.selectedIdeaComments = response.data;
+				//console.log($scope.selectedIdeaComments);
+			});
+
+	};
+
+	$scope.close = function() {
+		if ($scope.newIdeaShow) $scope.newIdeaShow = false;
+		if ($scope.shadowShow) $scope.shadowShow = false;
+		if ($scope.selectedIdeaShow) $scope.selectedIdeaShow = false;
+		if ($scope.adminShow) $scope.adminShow = false;
+	};
+
+	$scope.shadowClick = function() {
+		if ($scope.newIdeaShow) $scope.newIdeaShow = false;
+		if ($scope.shadowShow) $scope.shadowShow = false;
+		if ($scope.selectedIdeaShow) $scope.selectedIdeaShow = false;
+	};
+
+	$http.get("http://thinker-codeart.44fs.preview.openshiftapps.com/restapi/ideas/part/") //должны быть запрошены идеи текущего ползователя
+	.then(function(response){
+		$scope.ideas = response.data;
+		if ($scope.ideas.length > 19) $scope.moreIdeasBtn = true;
+		console.log($scope.ideas);
+        });
+
+});
