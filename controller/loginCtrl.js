6fd526f012b6fee10e3e367a1ba5eabee3f9ead7
Dmitry <area51m@i.ua>
2016-12-14 00:23:59 +0200

    Форма "забыл пароль" и выделяются лайки

--- controller/loginCtrl.js
+++ controller/loginCtrl.js
@@ -3,9 +3,16 @@ var app = angular.module("Thinker", []);
 app.controller('loginCtrl', function($scope) {
 	$scope.loginForm = true;
 	$scope.registerForm = false;
+	$scope.iForgot = false;
 
 	$scope.switchFormF = function() {
 		$scope.loginForm = !$scope.loginForm;
 		$scope.registerForm = !$scope.registerForm;
-	};	
+	};
+
+	$scope.iForgotSwitchF = function() {
+		$scope.loginForm = !$scope.loginForm;
+		$scope.iForgot = !$scope.iForgot;
+		console.log($scope.iForgot, 'YYY');
+	};		
 });
